ddf1d71705c288f2760b867fe052447fa8217c4a
luvnft <140175751+luvnft@users.noreply.github.com>
2023-12-05 02:00:50 -0500

    Update EventModal.tsx

--- frontend/src/components/EventModal/EventModal.tsx
+++ frontend/src/components/EventModal/EventModal.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, Image } from "@chakra-ui/react";
 
-const EventModal = () => {
+const EventModal = ({ isOpen, onClose, eventId }) => {
   return (
     <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Ticket for event #{._id}</ModalHeader>
+        <ModalHeader>Ticket for event #{eventId}</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
           <Image src="https://ipfs.io/ipfs/Qme1E49DEyeGRmT2CDeSCZRcNiLyeAQiDq7SAxDHACiivi/" />
